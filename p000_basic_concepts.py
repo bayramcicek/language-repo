f9a3f28a64cb517c1d0805a118759578fe8e9c63
bayramcicek <mail@bayramcicek.com.tr>
2018-07-16 23:36:43 +0300

    basics

---
+++ p000_basic_concepts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.6
+# created by cicek on 16.07.2018 22:06
+
+sum = 2 * (3+4)
+div = 10 / 2 # always float
+
+print(sum)
+print("float = " + str(div))
+
+print(1 + 2 + 3 + 4.0 + 5)
+print(9**(1/2)) # sqrt(9)
+
+print(20 // 7) # quotient
+print(13 % 6) # remainder
+
+print("hey\" how\' are you!")
+
+print('''Two
+Line''')
+
+print("Two\nLine")
+
+printMe = input("Enter a word: ") # input value is always converted to string automatically
+print(printMe)
