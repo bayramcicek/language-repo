17306bc05a73d7fecc356b9d1a02e3a79c8f4afd
bayramcicek <mail@bayramcicek.com.tr>
2018-09-14 20:41:29 +0300

    import p060__main__v3

---
+++ p061__main__v4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.6
+# created by cicek on 14.09.2018 20:20
+
+import p060__main__v3
+# This will always be run
+# Run From Import
+
+# print("Second Module's Name: {}".format(__name__))
+# Second Module's Name: __main__ # Python running this file directly
+
+p060__main__v3.func()
