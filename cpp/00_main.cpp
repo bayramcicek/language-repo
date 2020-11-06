14941b2d3daaca23a21080a88173e9810c0f77bd
bayramcicek <mail@bayramcicek.com.tr>
2020-11-06 20:45:01 +0300

    cpp week 01

---
+++ cpp/00_main.cpp
@@ -0,0 +1,12 @@
+// C11 standard
+// created by cicek on Nov 06, 2020 8:15 PM
+
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char **argv) {
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
